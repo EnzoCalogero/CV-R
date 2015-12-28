c5347a26dc6e9ec40b449aeb5755f9d79678eaca
enzo <enzus@hotmail.com>
2015-12-28 18:53:46 +0000

    modified2 yu

--- test.R
+++ test.R
@@ -1,20 +1,30 @@
-library(gplots)
-data(mtcars)
-x  <- as.matrix(mtcars)
-rc <- rainbow(nrow(x), start=0, end=.3)
-cc <- rainbow(ncol(x), start=0, end=.3)
+## app.R ##
+library(shinydashboard)
 
-## Show effect of row and column label rotation
-heatmap.2(x, srtCol=NULL)
-heatmap.2(x, srtCol=0,   adjCol = c(0.5,1) )
-heatmap.2(x, srtCol=45,  adjCol = c(1,1)   )
-heatmap.2(x, srtCol=135, adjCol = c(1,0)   )
-heatmap.2(x, srtCol=180, adjCol = c(0.5,0) )
-heatmap.2(x, srtCol=225, adjCol = c(0,0)   ) ## not very useful
-heatmap.2(x, srtCol=270, adjCol = c(0,0.5) )
-heatmap.2(x, srtCol=315, adjCol = c(0,1)   )
-heatmap.2(x, srtCol=360, adjCol = c(0.5,1) )
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+      
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      )
+    )
+  )
+)
 
-heatmap.2(x, srtRow=45, adjRow=c(0, 1) )
-heatmap.2(x, srtRow=45, adjRow=c(0, 1), srtCol=45, adjCol=c(1,2) )
-heatmap.2(x, srtRow=45, adjRow=c(0, 1), srtCol=270, adjCol=c(0,0.5) )
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
